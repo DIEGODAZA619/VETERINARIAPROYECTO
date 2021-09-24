3920a530998d2e7b0f899fbbd77247cb825adf5b
DIEGODAZA619 <diegodaza.da619@gmail.com>
2021-09-23 20:30:12 -0400

    INSERTAR USUARIOS

--- application/controllers/api/Login.php
+++ application/controllers/api/Login.php
@@ -10,65 +10,73 @@ class Login extends REST_Controller{
 		parent::__construct();
 		$this->objOfJwt = new CreatorJwt();
 		header('Content-Type: application/json');
-
 		$this->load->model('usuarios_model');
-
 	}
 
 	public function index_post()
 	{
 		$data = $this->post();
 
-		$username = $this->post("username");
-		$clave = $this->post("clave");
-		$clavemd5 = md5($clave);
-
-		$login = $this->usuarios_model->verificar_login($username,$clavemd5);
-		if($login)
-		{
-			if($login[0]->estadouser == 'AC')
+		if(array_key_exists('username', $data) && array_key_exists('clave', $data))			
+		{	
+			$username = $this->post("username");
+			$clave = $this->post("clave");
+			$clavemd5 = md5($clave);
+			$login = $this->usuarios_model->verificar_login($username,$clavemd5);//LLAMAR A UN MODELO
+			if($login)
 			{
-				$date = new DateTime();
-				$tokenData['idusuario'] = $login[0]->id_usuario;
-				$tokenData['fecha'] = $date->getTimestamp();
-				$jwtToken = $this->objOfJwt->GenerateToken($tokenData); //GENERA EL TOKEN
+				if($login[0]->estadouser == 'AC')
+				{
+					$date = new DateTime();
+					//VALORES PARA EL TOKEN
+					$tokenData['idusuario'] = $login[0]->id_usuario;
+					$tokenData['fecha'] = $date->getTimestamp();
 
-				$respuesta = array(
-							'error' => true,
-							'mensaje' => 'TOKEN',
-							'fecha' => $date->getTimestamp(),
-							'token'	  => $jwtToken
-						);
-				$this->response($respuesta, REST_Controller::HTTP_OK);	
+					$jwtToken = $this->objOfJwt->GenerateToken($tokenData); //GENERA EL TOKEN
+
+					$respuesta = array(
+								'error' => true,
+								'mensaje' => 'TOKEN',
+								'fecha' => $date->getTimestamp(),
+								'token'	  => $jwtToken    //devolvemos el token
+							);
+					$this->response($respuesta, REST_Controller::HTTP_OK);	
+				}
+				elseif ($login[0]->estadouser == 'EX')
+				{
+					$respuesta = array(
+								'error' => true,
+								'mensaje' => 'ACTUALICE LA CONTRASEÑA',
+								//'login'	  => $login
+							);
+					$this->response($respuesta, REST_Controller::HTTP_OK);	
+				}
+				elseif ($login[0]->estadouser == 'BA')
+				{
+					$respuesta = array(
+								'error' => true,
+								'mensaje' => 'USUARIO DESAHABILITADO',
+								//'login'	  => $login
+							);
+					$this->response($respuesta, REST_Controller::HTTP_NOT_FOUND);	
+				}
 			}
-			elseif ($login[0]->estadouser == 'EX')
+			else
 			{
 				$respuesta = array(
-							'error' => true,
-							'mensaje' => 'ACTUALICE LA CONTRASEÑA',
-							//'login'	  => $login
-						);
-				$this->response($respuesta, REST_Controller::HTTP_OK);	
+								'error' => true,
+								'mensaje' => 'Datos no Existentes',							
+							);
+				$this->response($respuesta, REST_Controller::HTTP_NOT_FOUND);
 			}
-			elseif ($login[0]->estadouser == 'BA')
-			{
-				$respuesta = array(
-							'error' => true,
-							'mensaje' => 'USUARIO DESAHABILITADO',
-							//'login'	  => $login
-						);
-				$this->response($respuesta, REST_Controller::HTTP_NOT_FOUND);	
-			}
-
-			
 		}
 		else
 		{
 			$respuesta = array(
-							'error' => true,
-							'mensaje' => 'Datos no Existentes',							
-						);
-			$this->response($respuesta, REST_Controller::HTTP_NOT_FOUND);
+								'error' => true,
+								'mensaje' => 'Debe introducir los parametros correctos',						
+							);
+			$this->response($respuesta, REST_Controller::HTTP_BAD_REQUEST);
 		}
 		
 		
--- application/controllers/api/Usuarios.php
+++ application/controllers/api/Usuarios.php
@@ -1,23 +1,110 @@
 <?php
 
 require APPPATH . '/libraries/REST_Controller.php';
+require APPPATH . '/libraries/CreatorJwt.php';
 
 class Usuarios extends REST_Controller {
   public function __construct(){
 		parent::__construct();
+		$this->objOfJwt = new CreatorJwt();
+		header('Content-Type: application/json');
 		$this->load->model('usuarios_model');
   }
 
   	public function getlistausuarios_get()
   	{
-  		$data = $this->usuarios_model->getusarios();
+  		$received_Token = $this->input->request_headers('Authorization'); //RECUPERAMOS EL TOKEN 
+  		if(array_key_exists('Authorization', $received_Token))  //VERIFICAMOS EL PARAMETRO DE Authorization
+  		{
+	  		$jwtData = $this->objOfJwt->DecodeToken($received_Token['Authorization']); //DECODIFICAMOS DATOS TOKEN
+	  		$iduser = $jwtData['idusuario']; // OBTENER VALORES DEL TOKEN
+	  		$data = $this->usuarios_model->getusarios();
+	  		if($data)
+	  		{
+	  			$respuesta = array(
+							'error' => false,
+							'mensaje' => 'Correcto, datos usuario',
+							'datos' => $data,
+							'token' => $jwtData,
+							'iduser' => $iduser
+					);
+			  	$this->response($respuesta, REST_Controller::HTTP_OK);	
+	  		}
+	  		else
+	  		{
+	  			$respuesta = array(
+							'error' => true,
+							'mensaje' => 'No se recupero ningun registro de usuarios',
+							'datos' => null,							
+					);
+			  	$this->response($respuesta, REST_Controller::HTTP_NOT_FOUND);	
+	  		}	  		
+		  }
+		  else
+		  {
+		  	$respuesta = array(
+							'error' => true,
+							'mensaje' => 'ACCESO DENEGADO',
+					);
+			  $this->response($respuesta, REST_Controller::HTTP_NOT_FOUND);
+		  }
 
+  	}
+
+  	function registrar_post()
+  	{
+  		$received_Token = $this->input->request_headers('Authorization'); //RECUPERAMOS EL TOKEN 
+  		if(array_key_exists('Authorization', $received_Token))  //VERIFICAMOS EL PARAMETRO DE Authorization
+  		{
+	  		$jwtData = $this->objOfJwt->DecodeToken($received_Token['Authorization']); //DECODIFICAMOS DATOS TOKEN
+	  		$iduser = $jwtData['idusuario']; // OBTENER VALORES DEL TOKEN
+	  		
+	  		$data = $this->post();
+	  		if (!(array_key_exists('nrodocumento', $data)
+	  					&& array_key_exists('nombres', $data)
+	  					&& array_key_exists('primer_apellido', $data)
+	  					&& array_key_exists('segundo_apellido', $data)
+	  					&& array_key_exists('tipo_usuario', $data)
+	  					&& array_key_exists('clave', $data)))
+	  		{
+	  			$respuesta = array(
+								'error' => true,
+								'mensaje' => 'Debe introducir los parametros correctos',						
+							);
+					$this->response($respuesta, REST_Controller::HTTP_BAD_REQUEST);	
+	  		}
+	  		else
+	  		{
+
+	  			//validacion de datos
+	  			$respuesta = $this->registrarusuario($data);
+	  			
+	  			/*$respuesta = array(
+							'error' => false,
+							'mensaje' => 'pruebaaa',
+							'datos' => $data,							
+					);*/
+			  	$this->response($respuesta, REST_Controller::HTTP_OK);	
+	  		}
+	  	}
+		  else
+		  {
+		  	$respuesta = array(
+							'error' => true,
+							'mensaje' => 'ACCESO DENEGADO',
+					);
+			  $this->response($respuesta, REST_Controller::HTTP_NOT_FOUND);
+		  }
+  	}
+  	function registrarusuario($data)
+  	{
   		$respuesta = array(
-						'error' => false,
-						'mensaje' => 'Correcto, datos usuario',
-						'datos' => $data
-				);
-		  $this->response($respuesta, REST_Controller::HTTP_OK);
+							'error' => false,
+							'mensaje' => 'pruebaaakjdkdjdj',
+							'datos' => $data,							
+			);
+			return $respuesta;
+
   	}
 }
 
